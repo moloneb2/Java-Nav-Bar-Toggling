05095cbebf6fa38f9cc8cfe4fe772a298e73c88c
moloneb2 <161696733+moloneb2@users.noreply.github.com>
2024-11-08 11:40:19 +0000

    toggling for a nav bar
    toggling for a nav bar

---
+++ java.js
@@ -0,0 +1,12 @@
+function NavToggle() {
+    document.getElementById("js-menu")
+    /*.classList.toggle("show");*/
+    .classList.toggle("hello")
+}
+
+/*function NavToggle() {
+    alert("hello")
+}*/
+const Display = 
+document.getElementById("js-menu")
+.addEventListener("click , ")
